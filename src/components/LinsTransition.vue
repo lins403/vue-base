3d09e7d861fc1ed4e14934a6694e6c10c5a03506
lins14 <linshan6264@gmail.com>
2021-11-17 20:55:57 +0800

    feat(components): 1

---
+++ src/components/LinsTransition.vue
@@ -0,0 +1,24 @@
+// https://cn.vuejs.org/v2/guide/transitions.html
+<script>
+/* eslint-disable */
+Vue.component('my-special-transition', {
+  functional: true,
+  render: function (createElement, context) {
+    var data = {
+      props: {
+        name: 'very-special-transition',
+        mode: 'out-in'
+      },
+      on: {
+        beforeEnter: function (el) {
+          // ...
+        },
+        afterEnter: function (el) {
+          // ...
+        }
+      }
+    }
+    return createElement('transition', data, context.children)
+  }
+})
+</script>
